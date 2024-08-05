b3076c5f20c88b9e5399598688a7412413703f2c
Melek Messoussi <125603818+MelekMessoussi@users.noreply.github.com>
2024-08-05 02:12:42 +0100

    Update voice.py

--- pages/voice.py
+++ pages/voice.py
@@ -14,8 +14,8 @@ AI71_API_KEY = 'api71-api-3c8094f7-5999-4d44-8f8b-be93bbccd82d'
 client = AI71(AI71_API_KEY)
 
 # Define constants for Eleven Labs API
-XI_API_KEY = 'sk_3b2ac2e63906d78ef6c9a8de065bb35addf7403cdcfd6fa0'
-VOICE_ID = 'XoTdo57oO5ozmnSwMYce'
+XI_API_KEY = 'sk_611a35236050d4c9b8db5b918b32837262ef5b47c46ea6c5'
+VOICE_ID = 'MDgMEREx7e3rFYpyQLdQ'
 CHUNK_SIZE = 1024
 OUTPUT_PATH = "./assets/speech.mp3"
 
